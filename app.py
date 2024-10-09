667ca86e0a725ab34223956b288baec92c3257a5
jwson <jwson@jwsons-MacBook-Pro.local>
2024-10-10 08:44:15 +0900

    Updated file

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from pages import chatbot, scheduler, report_generator
 
 from dotenv import load_dotenv
 
+# 페이지 설정 (상단의 'app'과 같은 기본 요소를 없애는 코드)
+st.set_page_config(page_title="KOGAS 업무용 AI 비서", layout="wide")
+
 __import__('pysqlite3')
 import sys
 
@@ -11,8 +14,6 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 load_dotenv()  # 이 함수를 호출하면 .env 파일의 내용이 환경 변수로 로드됩니다
 
-# 페이지 설정 (상단의 'app'과 같은 기본 요소를 없애는 코드)
-st.set_page_config(page_title="KOGAS 업무용 AI 비서", layout="wide")
 
 # 멀티페이지 앱 실행
 app = MultiApp()
